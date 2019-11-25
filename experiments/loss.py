1428a9c86bc966a4f1be50080fb191475ccf6205
Alasdair-P <alasdair.paren@gmail.com>
2019-11-25 22:51:27 +0000

    update loss

--- experiments/loss.py
+++ experiments/loss.py
@@ -93,3 +93,69 @@ class Distillation_Loss(nn.Module):
             # loss = loss_ce.mul(1-self.lambda_t) + loss_dist.mul(self.lambda_t)
             loss = loss_ce.mean() + loss_dist.mul(self.lambda_t).mean()
         return loss, loss_dist.mean()
+
+'''
+class Distillation_Loss_2(nn.Module):
+
+    def __init__(self, args):
+        super(Distillation_Loss_2, self).__init__()
+
+        print('Creating distillation loss version two')
+
+        load_model = args.load_model
+        model_arch = args.model
+        depth = args.depth
+
+        args.load_model = args.teacher
+        if 'spiral' in args.teacher:
+            args.depth = 5
+            args.width = 50
+        elif '20' in args.teacher:
+            args.depth = 20
+        elif '32' in args.teacher:
+            args.depth = 32
+        elif '44' in args.teacher:
+            args.depth = 44
+        elif '56' in args.teacher:
+            args.depth = 56
+        elif '110' in args.teacher:
+            args.depth = 110
+        else:
+            print('unknown teacher architecture')
+            raise NotImplementedError
+
+        self.teacher_model = get_model(args)
+        self.teacher_model.eval()
+
+        print(args.load_model, load_model)
+        # print(args.load_model, load_model)
+        args.load_model = load_model
+        # print(args.load_model, load_model)
+        # input('press any key')
+        args.model = model_arch
+        args.depth = depth
+
+        self.lambda_t = args.lambda_t
+        self.tau = args.tau
+        self.lower_bound = torch.zeros(arg.train_size).fill_(args.B)
+
+        self.loss = nn.CrossEntropyLoss()
+
+        self.kl = False
+        if args.loss == 'kl':
+            self.kl = True
+
+    def forward(self, scores, y, x):
+        with torch.no_grad():
+            scores_teacher = self.teacher_model(x).detach()
+        loss_ce = self.loss(scores, y)
+        loss_dist = -(F.softmax(scores_teacher.div(self.tau),dim=1).mul( F.log_softmax(scores.div(self.tau),dim=1) - F.log_softmax(scores_teacher.div(self.tau),dim=1) )).sum(dim=1)
+
+        if self.kl:
+            loss = loss_dist.clamp(min=self.lower_bound).mean()
+        else:
+            # loss = loss_ce.mul(1-self.lambda_t) + loss_dist.mul(self.lambda_t)
+            loss = loss_ce.mean() + loss_dist.mul(self.lambda_t).mean()
+
+        return loss, loss_dist.mean()
+'''
