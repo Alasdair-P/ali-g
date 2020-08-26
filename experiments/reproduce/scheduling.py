0017929ffa4711b780ae72a2f625a9a593f08f8c
Alasdair-P <alasdair.paren@gmail.com>
2020-08-26 17:14:28 +0100

    move svhn to jade

--- experiments/reproduce/scheduling.py
+++ experiments/reproduce/scheduling.py
@@ -25,8 +25,8 @@ def run_command(command, on_gpu, noprint):
         # else:
         try:
             # waitGPU.wait(nproc=0, interval=1, ngpu=1, gpu_ids=[0, 1, 2, 3])
-            waitGPU.wait(nproc=0, interval=1, ngpu=1, gpu_ids=[3])
-            # waitGPU.wait(nproc=0, interval=1, ngpu=1, gpu_ids=[0, 1])
+            # waitGPU.wait(nproc=0, interval=1, ngpu=1, gpu_ids=[3])
+            waitGPU.wait(nproc=0, interval=1, ngpu=1, gpu_ids=[0, 1])
             print(f'GPUS IDS SET TO {gpu_ids}')
         except:
             print("Failed to run `waitGPU.wait` --> no automatic scheduling on GPU")
